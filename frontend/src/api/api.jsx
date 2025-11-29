a36895425a19894db075fd097cc36cfde34adce7
Jean Rocha <jean.qrocha96@gmail.com>
2025-11-28 21:06:44 -0300

    feat: update API base URL to production environment

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({ //cria uma instância do axios
-    baseURL: "http://localhost:3000", //define a URL base da API
+    baseURL: "https://agenda-4-neni.onrender.com", //define a URL base da API
 })
 
 api.interceptors.request.use( //adiciona um interceptor para requisições
