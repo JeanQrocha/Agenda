fba487fdbada7d2a63292ce65ae48b9a02a672ce
Jean Rocha <jean.qrocha96@gmail.com>
2025-12-02 21:35:42 -0300

    fix: add auth middleware to atendimento routes for enhanced security

--- backend/src/router/atendimento.js
+++ backend/src/router/atendimento.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import ControllerAtendimento from '../controller/atendimento.js'
+import authMiddleware from '../middleware/auth.js'
 
 const router = express.Router()
 
-router.get('/atendimentos', ControllerAtendimento.FindAll)
-router.get('/atendimento/:id', ControllerAtendimento.FindOne)
-router.post('/atendimento', ControllerAtendimento.Create) 
-router.put('/atendimento/:id', ControllerAtendimento.Update) 
-router.delete('/atendimento/:id', ControllerAtendimento.Delete)
+router.get('/atendimentos',authMiddleware(), ControllerAtendimento.FindAll)
+router.get('/atendimento/:id', authMiddleware(), ControllerAtendimento.FindOne)
+router.post('/atendimento', authMiddleware(), ControllerAtendimento.Create) 
+router.put('/atendimento/:id',authMiddleware(), ControllerAtendimento.Update) 
+router.delete('/atendimento/:id',authMiddleware(), ControllerAtendimento.Delete)
 
 
 export default router
