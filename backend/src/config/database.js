fa9ff50e85dfa8c0ed3923b1c14ffb2e70a6d7e4
Jean Rocha <jean.qrocha96@gmail.com>
2025-12-05 19:35:48 -0300

    fix: update database configuration to use PostgreSQL and change API base URL for production

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,11 +8,19 @@ class database {
 
     init() {
         this.db = new Sequelize({
-            database: "agenda",
-            host: 'localhost',
-            username: 'root',
-            password: '',
-            dialect: 'mysql'
+            database: "agenda_8ftd",
+            host: 'dpg-d4l286be5dus73fhifkg-a',
+            username: 'agenda_8ftd_user',
+            password: 'YdYjoYPR4aFCfdz4YR10qYCTGlbXR2ci',
+            dialect: 'postgres',
+            protocol: 'postgres',
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            },
+            logging: false
 
         })
     }
